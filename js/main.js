bf2f3285615789d214c2025cb60ef91f8ed13f0a
Alejok0 <ale.ghost32@gmail.com>
2022-08-18 15:59:25 -0500

    Mejora en la maquetación de secciones importantes.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+'use strict'
+
+window.addEventListener('load', function () {
+
+  function enviarCorreo() {
+    /*
+      Esta funcion servirá para enviar un correo de contacto.
+      window.open('mailto:test@example.com?subject=subject&body=body');
+     */
+  }
+});
