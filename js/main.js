3466de1cba9f2d754c2ed7aba2729fe401539d92
Alejok0 <ale.ghost32@gmail.com>
2022-08-26 14:18:48 -0500

    Soluciono bugs
    - Centro el menu superior

--- js/main.js
+++ js/main.js
@@ -17,14 +17,14 @@ $(document).ready(function () {
     limpiarPaginaActual(contenido);
     $("#estiloActual").attr("href", "css/proyectos.css");
     $(contenido).addClass("contenedor");
-    $(contenido).load("proyectos.html");
+    $(contenido).load("https://alejok0.github.io/proyectos.html");
   })
 
   $("#acercaDe").click(function () {
     limpiarPaginaActual(contenido);
     $("#estiloActual").attr("href", "css/acercaDe.css");
     $(contenido).addClass("contenedor");
-    $(contenido).load("acercaDe.html");
+    $(contenido).load("https://alejok0.github.io/acercaDe.html");
   });
 
 });
